cd901eac89de407edafbd88640b2b14f4955167d
jekaantipin <jekaantipin69@gmail.com>
2015-08-26 15:25:43 +0300

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,15 +29,20 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
   config.assets.compress = true
-  # config.assets.js_compressor = :closure
+  config.assets.compile = false
+  config.assets.js_compressor = :closure
   config.assets.css_compressor = :yui
+
+  # config.assets.compress = true
+  # config.assets.js_compressor = :closure
+  # config.assets.css_compressor = :yui
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
