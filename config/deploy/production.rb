0a67532e66705c382715dc4674c9ff16d4b2b002
jekaantipin <jekaantipin69@gmail.com>
2016-07-14 16:41:45 +0300

    update ruby production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -18,7 +18,8 @@ server "5.101.99.155", :app, :web, :db, :primary => true
 
 # Используем rvm
 set :using_rvm, true
-set :rvm_ruby_string, 'ruby-2.2.1'
+set :rvm_ruby_string, 'ruby-2.2.4'
+
 set :rvm_type, :user
 
 # Авторизационные данные
